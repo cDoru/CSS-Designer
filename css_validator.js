c728b1b0ae8d529080986807a992510a3923241f
Peter Hoffmann <Hoffmann.P@gmx.net>
2012-12-07 16:50:27 +0100

    update 1

--- css_validator.js
+++ css_validator.js
@@ -1,4 +1,4 @@
-/*jslint es5: true, forin: true, indent: 4, plusplus: true, vars: true */
+/*jslint es5: true, forin: true, indent: 4, plusplus: true, vars: true, regexp: true */
 /*global define, window */
 
 define(
@@ -6,29 +6,71 @@ define(
 	function (error, css_properties) {
 		'use strict';
 
-		var css_validator = {};
-
-		this.css_validator = function (who, what, how) {
-			return window.console.log("Hallo");
+		var css_validator = {
+			whats: []
 		};
 
-		this.validate_property = function (what, how) {
-			var i, prop = {}, props = [], expanders;
-			if (css_properties.properties.hasOwnProperty(what) || css_properties.page_properties.hasOwnProperty(what)) {
-				prop[what] = how;
-				props.append(prop);
-				return props;
-			}
-			if (css_properties.compounds.hasOwnProperty(what)) {
-				expanders = css_properties.compounds[what];
-				if (expanders.hasOwnProperty(0) !== "string") {
-					expanders = expanders[how.split(' ').length];
+		css_validator.prototype.read = function (who, what, how) {
+			this.whats = this.property_decompund(what, how);
+			this.whats.forEach(this.validate);
+			this.whats.filter(
+				function (value) {
+					return value === null;
 				}
-				for (i = 0; i < expanders.length; i++) {
-					props.append(this.expand_property(what, expanders[i], how));
-				}
-				return props;
+			);
+		};
+
+		// CSS-Sammlung (z.B.: font) oder Zusammenfassung (zB.: border-width) auflösen
+		css_validator.prototype.property_decompund = function (what, how) {
+			var i,
+				expander,
+				expanders = css_properties.compounds[what],
+				props = [],
+				all_compounds = function (expand_to, i, props) {
+					if (expand_to[0] !== '\\') {
+						what += '-' + expand_to;
+					} else {
+						what = expand_to.substr(1, expand_to.lenght - 1);
+					}
+					props[i] = [what, how];
+				};
+			if (!css_properties.compounds.hasOwnProperty(what)) {
+				return [[what, how]];
 			}
+
+			// dieseRegEx kann die folgenden zusammengesetzten CSS-Werte richtig spalten:
+			// "italic 15px 'Times New Roman', Times, serif"       -> ["italic",  "15px",     "'Times New Roman', Times, serif"]
+			// "bullet outside url('http://url mit whitespace.com')" -> ["bullet",  "outside",  "url('http://url mit whitespace.com')"]
+			how = how.match(/(url\(("[^"]*"|'[^']*')\)|((['"]\w+(\W+\w+)*['"]|\w+)(,\W+(['"]\w+(\W+\w+)*['"]|\w+))*)|\w+)/ig);
+			expanders = css_properties.compounds[what];
+			for (i = 0; i < how.length; i++) {
+				// Vorarbeit für Zusammenfassungen abhängig von der Anzahl der Werte
+				if (!expanders.hasOwnProperty(0)) {
+					expander = expanders[how.length][i];
+					if (typeof expander === 'string') {
+						expander = [expander];
+					}
+				}
+				expander.forEach(all_compounds);
+				[].push(props, expander);
+			}
+
+			/* hier muss noch der font-size/line-height-hook rein */
+			return props;
+		};
+
+		css_validator.prototype.validate = function (pair, index, whats) {
+			var what = pair[0], how = pair[1];
+
+			// property validierung
+			if (!(css_properties.properties.hasOwnProperty(what) || css_properties.page_properties.hasOwnProperty(what))) {
+				whats[index] = null;
+				return null;
+			}
+
+
+			// value validierung
+
 		};
 
 		return css_validator;
